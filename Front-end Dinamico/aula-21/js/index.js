859ea8bd825d19087d8d31353afac1fa513bf725
laraalvesfreitas <laraalves624@gmail.com>
2026-03-27 20:17:52 -0300

    Parte 3 aula 21

--- Front-end Dinamico/aula-21/js/index.js
+++ Front-end Dinamico/aula-21/js/index.js
@@ -6,25 +6,25 @@ const input = document.querySelector("#input");
 
 
 //Eventos no dom (Programação orientada a eventos)
-    
-    botaoAumentar.addEventListener('click', (event) =>{
-        const valorAtual = Number(contadorElemento.textContent);
-        contadorElemento.textContent = valorAtual + 1;
-        botaoAumentar.classList.add("btn"); //Adiciona a classe "btn" ao elemento
-        botaoDiminuir.classList.remove("btn"); //Remove a classe "btn" do elemento
-    });
 
-    botaoDiminuir.addEventListener('click', (event) =>{
-        const valorAtual = Number(contadorElemento.textContent);
-        contadorElemento.textContent = valorAtual - 1;
+botaoAumentar.addEventListener('click', (event) => {
+    const valorAtual = Number(contadorElemento.textContent);
+    contadorElemento.textContent = valorAtual + 1;
+    botaoAumentar.classList.add("btn"); //Adiciona a classe "btn" ao elemento
+    botaoDiminuir.classList.remove("btn"); //Remove a classe "btn" do elemento
+});
 
-        botaoDiminuir.classList.add("btn"); //Adiciona a classe "btn" ao elemento
-        botaoAumentar.classList.remove("btn"); //Remove a classe "btn" do elemento
-    });
+botaoDiminuir.addEventListener('click', (event) => {
+    const valorAtual = Number(contadorElemento.textContent);
+    contadorElemento.textContent = valorAtual - 1;
 
-    input.addEventListener('input', () =>{
-        console.log(input.value);
-    });
+    botaoDiminuir.classList.add("btn"); //Adiciona a classe "btn" ao elemento
+    botaoAumentar.classList.remove("btn"); //Remove a classe "btn" do elemento
+});
+
+input.addEventListener('input', () => {
+    console.log(input.value);
+});
 
 //Adicionando estilos inline no elemento contador
 contadorElemento.style.color = "red";
@@ -42,18 +42,36 @@ console.log(botaoDiminuir.classList); //Lista de classes do elemento
 
 // Manipular o tema da página (Branco e Preto)
 const themeButton = document.querySelector("#theme");
-let darkTheme = false;
+let darkTheme;
 
-themeButton.addEventListener('click', () =>{
-    darkTheme = !darkTheme; //Inverte o valor de darkTheme (true para false e vice-versa)
+function theme() {
     const body = document.querySelector("body");
 
-    if(darkTheme){
-    body.style.backgroundColor = 'black';
-    body.style.color = 'white';
-    }else{
-    body.style.backgroundColor = 'white';
-    body.style.color = 'black';
+    if (darkTheme) {
+        body.style.backgroundColor = 'black';
+        body.style.color = 'white';
+    } else {
+        body.style.backgroundColor = 'white';
+        body.style.color = 'black';
     }
+}
+
+// * Definindo uma função que será executada ao carregar o conteúdo da página/janela
+window.onload = () => {
+    const isDarkThemeLocalStorage = localStorage.getItem('isDarkTheme');
+    darkTheme = isDarkThemeLocalStorage === 'true';
+
+    theme(); //Chama a função para aplicar o tema com base no valor armazenado no localStorage
+};
+
+
+themeButton.addEventListener('click', () => {
+    darkTheme = !darkTheme; //Inverte o valor de darkTheme (true para false e vice-versa)
+
+    localStorage.setItem('isDarkTheme', darkTheme); //Armazena o tema no localStorage
+
+    theme(); //Chama a função para aplicar o tema com base no valor atualizado de darkTheme
+
 });
+
 
