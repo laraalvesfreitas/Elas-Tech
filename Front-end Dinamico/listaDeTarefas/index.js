1247fa39437b010ba7597cb3c19f703e34730f0b
laraalvesfreitas <laraalves624@gmail.com>
2026-04-08 12:20:22 -0300

    Lista de Tarefas v3

--- Front-end Dinamico/listaDeTarefas/index.js
+++ Front-end Dinamico/listaDeTarefas/index.js
@@ -8,59 +8,48 @@ const todoListUl = document.querySelector('#todo-list');
 
 let tasks = []; // [{title: 'Tarefa 1', done: false}, {title: 'Tarefa 2', done: true}]
 
-// Lidando com eventos 
-form.addEventListener('submit', (event) => {
-    event.preventDefault(); // Evita o comportamento padrão do formulário (recarregar a página)
-    const taskTitle = taskTitleInput.value
-
-    if (taskTitle.length < 3) {
-        alert('A tarefa deve conter pelo menos 3 caracteres.');
-        return;
-    }
-
-    // Adicionando a nova tarefa ao array de tarefas
-    tasks.push({
-        title: taskTitle,
-        done: false
-    }); // Adiciona a nova tarefa ao objeto de tarefas
-
-
-    //Adicionando a nova tarefa no HTML
+function renderTasksOnHtml(taskTitle, done = false) {
     const li = document.createElement('li');
 
     const input = document.createElement('input');
     input.setAttribute('type', 'checkbox'); // Atribuindo o tipo checkbox ao input
-    
+
     input.addEventListener('change', (event) => {
-        const liToggle = event.target.parentElement; 
+        const liToggle = event.target.parentElement;
 
         const spanToToggle = liToggle.querySelector('span'); // Obtendo o elemento <span> dentro do <li> para alterar o estilo
 
         const done = event.target.checked; // Verificando se o checkbox está marcado ou não
-        if(done){
+        if (done) {
             liToggle.style.textDecoration = 'line-through'; // Se estiver marcado, risca o texto da tarefa
-        }else{
+        } else {
             liToggle.style.textDecoration = 'none'; // Se não estiver marcado, remove o risco do texto da tarefa
         }
 
         tasks = tasks.map(t => {
-            if(t.title === spanToToggle.textContent){
+            if (t.title === spanToToggle.textContent) {
                 return {
-                    title : t.title,
-                    done : !t.done,
-            }
+                    title: t.title,
+                    done: !t.done,
+                }
             }
 
             return t;
 
+        })
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+
+
     });
+    input.checked = done; 
 
-    console.log(tasks);
-
-});
+    
 
     const span = document.createElement('span');
     span.textContent = taskTitle; // Definindo o texto do span como o título da tarefa
+    if(done) {
+        span.style.textDecoration = 'line-through';
+    }
 
     const button = document.createElement('button');
     button.textContent = 'Remover'; // Definindo o texto do botão como "Remover"
@@ -74,7 +63,7 @@ form.addEventListener('submit', (event) => {
         todoListUl.removeChild(liToRemove); // Removendo a tarefa do HTML
 
 
-        console.log(tasks);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
     });
 
 
@@ -83,10 +72,46 @@ form.addEventListener('submit', (event) => {
     li.appendChild(span);
     li.appendChild(button);
 
-    
+
 
     todoListUl.appendChild(li); // Adicionando a nova tarefa à lista no HTML
+}
+    
+// Renderizando as tarefas salvas no localStorage ao carregar a página
+const tasksOnLocalStorage = localStorage.getItem('tasks');
+window.onload = () =>{
+
+    if(!tasksOnLocalStorage) return;
+
+    tasks = JSON.parse(tasksOnLocalStorage); 
+    tasks.forEach(t => {
+        renderTasksOnHtml(t.title, t.done);
+})
+}
+    
+    // Lidando com eventos 
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // Evita o comportamento padrão do formulário (recarregar a página)
+    const taskTitle = taskTitleInput.value
+
+    if (taskTitle.length < 3) {
+        alert('A tarefa deve conter pelo menos 3 caracteres.');
+        return;
+    }
+
+    // Adiciona a nova tarefa ao objeto de tarefas    
+    tasks.push({
+        title: taskTitle,
+        done: false
+    });
+
+    // Adicionando no localStorage
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    //Adicionando a nova tarefa no HTML
+    renderTasksOnHtml(taskTitle);
 
     taskTitleInput.value = '';
 
 });
+
