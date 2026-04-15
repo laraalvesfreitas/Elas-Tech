0e2fc8a51030ff04e6ba1c8a1ac20eb0072fb6f5
laraalvesfreitas <laraalves624@gmail.com>
2026-04-15 20:04:02 -0300

    Primeiro projeto c3

--- React/primeiro-projeto/src/components/Article/Article.jsx
+++ React/primeiro-projeto/src/components/Article/Article.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import articleImg from '../../assets/images/article1.png'
 import "./style.css";
 
 /*
@@ -12,14 +11,14 @@ export class Article extends React.Component{
     render(){
         return(
             <article id="article">
-                <img src={articleImg} alt="" id="imgArticle1" />
+                <img src={this.props.thumbnail} alt={this.props.title} id="imgArticle1" />
 
                 <div className="article-infos">
 
                 <h2>{this.props.title}</h2>
-                <h3>NASA</h3>
+                <h3>{this.props.provider}</h3>
 
-                <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+                <p>{this.props.description}</p>
 
                 </div>
             </article>
