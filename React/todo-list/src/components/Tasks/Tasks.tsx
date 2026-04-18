a8b17d55fc68af756b5b33a5992d90e4c33634ee
laraalvesfreitas <laraalves624@gmail.com>
2026-04-18 16:19:39 -0300

    Lógica de adicionar Task

--- React/todo-list/src/components/Tasks/Tasks.tsx
+++ React/todo-list/src/components/Tasks/Tasks.tsx
@@ -1,19 +1,61 @@
-import React from "react"
+import React, { SubmitEvent, useState } from "react"
 import styles from './style.module.scss'
 
+interface Task{
+    title: string;
+    done: boolean;
+    id: number;
+}
+
 export const  Tasks:  React.FC = () => {
+    const [taskTitle, setTaskTitle] = useState('')
+    const [tasks, setTasks] = useState([] as Task [])
+
+// Função disparada quando o usuário está querendo adicionar uma nova tarefa
+    function handleSubmitAddTask(event: SubmitEvent){
+        event.preventDefault()
+        if(taskTitle.length  < 3){
+            alert ('Não é possivel adicionar uma tarefa com menos de 3 caracteres.')
+            return;
+        }
+
+        // Adiciona a tarefa
+        setTasks([
+            ...tasks, // Pega todas as tarefas que já existem e coloca nesse novo valor do estado de tarefas
+            {id:new Date().getTime(), title: taskTitle, done: false},
+        ])
+        setTaskTitle('')
+
+    }
+
     return(
         <section className={styles.container}>
 
-        <form>
+        <form onSubmit={handleSubmitAddTask}>
             <div>
                 <label htmlFor="task-title">Nova Tarefa</label>
-                <input type="text"  id="task-title" placeholder="Título da Tarefa"/>
+                <input value={taskTitle}  onChange={(event) => setTaskTitle(event.target.value)}
+                type="text"  
+                id="task-title" 
+                placeholder="Título da Tarefa"/>
             </div>
 
-            <button>Adicionar Tarefa</button>
+            <button type="submit">Adicionar Tarefa</button>
 
         </form>
+
+        <ul>
+            {tasks.map(task => {
+                return(
+                    <li key={task.id}>
+                        <input type="checkbox" id={`task-${task.id}`}/>
+                        <label htmlFor={`task-${task.id}`}>{task.title}</label>
+                    </li>
+                )
+            })}
+           
+            
+        </ul>
 
         </section>
     )
