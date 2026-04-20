ce4d5caec55112df350d65eb00b261756714b9bb
laraalvesfreitas <laraalves624@gmail.com>
2026-04-20 11:23:46 -0300

    Removendo pasta Conceps

--- React/todo-list/src/App.tsx
+++ React/todo-list/src/App.tsx
@@ -1,8 +1,6 @@
 import { Header } from './components/Header/Header';
 import "./styles/global.css"
 import { Tasks } from './components/Tasks/Tasks';
-import { Refs } from './components/Concepts/Refs';
-import { Memoization } from './components/Concepts/Memoization';
 
 function App() {
   return (
@@ -10,10 +8,7 @@ function App() {
     <>
     <Header/>
     <Tasks/>
-
-    {/* <Refs/> */}
-    <Memoization financialData={{incomes: [50, 30, 40], outcomes: [80,90,10]}}/>
-    
+   
     </>
    
   );
