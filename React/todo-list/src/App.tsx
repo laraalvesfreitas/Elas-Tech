da3f78b2c136772277395139445ee3b57bbc5732
laraalvesfreitas <laraalves624@gmail.com>
2026-04-19 22:14:04 -0300

    Exemplo useCallback

--- React/todo-list/src/App.tsx
+++ React/todo-list/src/App.tsx
@@ -2,6 +2,7 @@ import { Header } from './components/Header/Header';
 import "./styles/global.css"
 import { Tasks } from './components/Tasks/Tasks';
 import { Refs } from './components/Concepts/Refs';
+import { Memoization } from './components/Concepts/Memoization';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Tasks/>
 
     {/* <Refs/> */}
+    <Memoization financialData={{incomes: [50, 30, 40], outcomes: [80,90,10]}}/>
     
     </>
    
--- React/todo-list/src/components/Concepts/Memoization.tsx
+++ React/todo-list/src/components/Concepts/Memoization.tsx
@@ -1,19 +1,54 @@
-import React from "react";
+import React, { useCallback, useMemo, useState } from "react";
 
 interface MemoizationProps{
     financialData:{
-        incomes : number[],
-        outcomes : number[], 
+        incomes : number[];
+        outcomes : number[]; 
     }
 }
 
 
-export const Memoization : React.FC <MemoizationProps> = (financialData) =>{
+
+export const Memoization : React.FC<MemoizationProps> = ({financialData}) =>{
+   //Estado que define se os valores devem ser mostrados ou não
+    const [showValues, setShowValues] = useState(true)
+
+    const totalIncomes = useMemo(()=>{
+           return financialData.incomes.reduce((total, income) =>{
+            console.log('Calculando o total de recebimentos...')
+            return total += income
+        }, 0)
+    }, [financialData.incomes])
+
+
+    const totalOtcomes = useMemo(() =>{
+        return  financialData.outcomes.reduce((total, outcome) => {
+        console.log('Calculando o total das despesas...')
+        return total += outcome
+        }, 0)
+    }, [financialData.outcomes])
+
+    
+
+    const aplicarDesconto = useCallback((desconto:number) =>{
+        return totalOtcomes * (1 - desconto)
+    },[totalOtcomes]) 
+
     return(
-        <div>
+        <div style={{padding:'2rem'}}>
             <h1>Memoization</h1>
 
             <h2>useMemo</h2>
+
+            <p>{`Total de receitas R$ ${showValues? totalIncomes : 'XXX'}`}</p>
+            <br />
+            <p>{`Total de despesas R$ ${showValues ? totalOtcomes : 'XXX'}`}</p>
+
+            <br />
+            <br />
+            <button onClick={() => {setShowValues(!showValues )}}>
+                {showValues ? 'Ocultar Valores' : 'Mostrar Valores'}
+                </button>
         </div>
     )
 }
